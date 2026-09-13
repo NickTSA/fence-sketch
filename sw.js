696f5909af87a5115d9c506c8c400efebf936d39
Narek Tsaturyan <45651986+NickTSA@users.noreply.github.com>
2026-09-12 21:02:13 -0700

    bug fix
    bug fix when drawing lines on mobile.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* Fence Sketch offline cache - built 574ac3a82b */
-var CACHE = "fence-sketch-574ac3a82b";
+/* Fence Sketch offline cache - built be16927698 */
+var CACHE = "fence-sketch-be16927698";
 var SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(e){
